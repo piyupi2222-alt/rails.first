2dbde03b80ab2da48bcbcb5081e09640eb967c0c
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-17 21:54:25 +0900

    schedul

--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,10 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    #   config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.time_zone = "Tokyo"
-    config.active_record.default_timezone = :local
+  
   end
 end
