07f8d41311585dbf25342b104ee87e1c99562da8
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-18 21:20:30 +0900

    add

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,52 +1,50 @@
 class SchedulesController < ApplicationController
-  def index
-    @schedules = Schedule.all
-  end
+    def index
+      @schedules = Schedule.all
+    end
+   
+    def create
+      @schedule = Schedule.new(params.require(:schedule).permit(:title, :startday, :finishday, :allday, :updateday, :schedule_memo))
+      if @schedule.save
+        flash[:notice] = "予定を追加しました"
+        redirect_to :schedules
+      else
+        render "new"
+      end
+    end
 
-  def new
-    @schedule = Schedule.new
-  end
+    def show
+      @schedule = get_params
+    end
+  
+    def new
+      @schedule = Schedule.new
+    end
+  
+    def edit
+      @schedule = get_params
+    end
+  
+    def update
+      @schedule = get_params
+      if @schedule.update(params.require(:schedule).permit(:title, :startday, :finishday, :allday, :updateday, :schedule_memo))
+        flash[:notice] = "スケジュールを更新しました"
+        redirect_to :schedules
+      else
+        render "edit"
+      end
+    end
 
-  def create
-    @schedule = Schedule.new(schedule_params)
-    if @schedule.save
-      flash[:notice] = "スケジュールを新規登録しました"
+    def destroy
+      @schedule = get_params
+      @schedule.destroy
+      flash[:notice] = "予定を削除しました"
       redirect_to :schedules
-    else
-      flash[:alert] = "スケジュールの情報を登録できませんでした"
-      render "new"
     end
+
+    private
+      def get_params
+        Schedule.find(params[:id])
+      end
   end
-
-  def show
-    @schedule = Schedule.find(params[:id])
-  end
-
-  def edit
-    @schedule = Schedule.find(params[:id])
-  end
-
-  def update
-    @schedule = Schedule.find(params[:id])
-    if @schedule.update(schedule_params)
-      flash[:notice] = "スケジュールIDが「#{@schedule.id}」の情報を更新しました"
-      redirect_to :root
-    else
-      flash[:alert] = "スケジュールIDが「#{@schedule.id}」の情報を更新できませんでした"
-      render "edit"
-    end
-  end
-
-  def destroy
-    @schedule = Schedule.find(params[:id])
-    @schedule.destroy
-    flash[:notice] = "スケジュールを削除しました"
-    redirect_to :root
-  end
-
-  private
-
-    def schedule_params
-      params.require(:schedule).permit(:title, :start_date, :end_date, :all_day, :schedule_memo)
-    end
-end
+  
