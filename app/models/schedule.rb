39d7d90992049cf94e4629b3092d1051d5db33d5
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-18 21:22:58 +0900

    add

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,19 +1,19 @@
-class Schedule < ApplicationRecord
-  validates :title, presence: true
-  validates :title, length: { maximum: 20 }
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :schedule_memo, length: { maximum: 500 }
+cclass Schedule < ApplicationRecord
+    validates :title, presence:true, length:{maximum:20}
+    validates :startday, presence:true
+    validates :finishday, presence:true
+    validates :schedule_memo, length:{maximum:500}
+    validate :startday_finishday_check
 
-  validate :end_date_after_start_date
-
-  def end_date_after_start_date
-    if end_date == nil
-
-    elsif end_date < Date.tomorrow
-      errors.add(:end_date,"は開始日以降の日付を選択してください")
-    else
-
+    def startday_finishday_check
+        if startday == nil
+            return
+        elsif finishday == nil
+            return
+        elsif self.startday >= self.finishday
+            errors.add(:finishday, "は開始日以降の日付を選択してください。")
+        else
+            return
+        end
     end
-  end
 end
